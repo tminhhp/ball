a5f4209f6aea4ba3a8edbadafcde5b7dff1a2b39
tranminh <tminhhp@gmail.com>
2013-01-28 08:50:40 +0700

    abc

---
+++ ball.js
@@ -0,0 +1,70 @@
+function Ball(radius, x, y) {
+    if (typeof radius == "number") {
+        this.radius = radius;
+    }
+    if (typeof x == "number") {
+        this.x = x;
+    }
+    if (typeof y == "number") {
+        this.y = y;
+    }
+    this.speedX = 5;
+    this.speedY = 5;
+    this.anpha = 1;
+}
+
+Ball.prototype.setRadius = function (radius) {
+    if (typeof radius == "number"){
+        this.radius = radius;
+        return true;
+    } else {
+        return false;
+    }
+}
+
+Ball.prototype.setSpeadX = function (speadX) {
+    if (typeof speadX == "number") {
+        this.speedX = speadX;
+        return true;
+    } else {
+        return false;
+    }
+}
+
+Ball.prototype.setSpeadY = function (speadY) {
+    if (typeof speadY == "number") {
+        this.speedX = speadY;
+        return true;
+    } else {
+        return false;
+    }
+}
+
+Ball.prototype.setX = function (x) {
+
+}
+
+Ball.prototype.setY = function (x) {
+
+}
+
+Ball.prototype.getX = function () {
+
+}
+
+Ball.prototype.getY = function () {
+
+}
+
+Ball.prototype.move = function () {
+    this.x += this.speedX;
+    this.y += this.speedY;
+}
+
+Ball.prototype.draw = function (context) {
+    context.beginPath();
+    context.fillStyle = "rgba(255, 100, 100," + this.anpha + ")";
+    context.arc(this.x, this.y, this.radius, 0, 2*Math.PI);
+    context.fill();
+    context.closePath();
+}
---
+++ test.php
@@ -0,0 +1,9 @@
+<?php
+
+echo "Test PHP";
+for ($i=0; $i < 10; $i++) { 
+	# code...
+	echo "Vãi thật đấy :))";
+}
+
+?>
